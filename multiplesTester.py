6ef864309e1b2129fbae815529219ec68d68ab03
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-07-26 14:38:18 +0300

    updated the multiplesTester.py with all imports from my multiples.py module

--- multiplesTester.py
+++ multiplesTester.py
@@ -1,3 +1,9 @@
 import multiples
-
+print("Multiples of Two \n")
 multiples.multiples_Of_Two()
+print("\n Multiples of Four\n")
+multiples.multiples_Of_Four()
+print("\n Multiples of Nine")
+multiples.multiples_Of_Nine()
+print("\n Multiples of Three")
+multiples.multiples_Of_Three()
