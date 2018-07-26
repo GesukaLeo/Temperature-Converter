823980081c6ab4595298bea54d493fa66aaad2dc
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-07-26 14:50:17 +0300

    updated the multiples module to print the numbers in a single file

--- multiples.py
+++ multiples.py
@@ -4,25 +4,25 @@
 def multiples_Of_Two():
     for number in range(1,101):
         if(number%2==0):
-            print(number)
+            print(number,end=' ')
 
 
 #get multiples of Three
 def multiples_Of_Three():
     for number in range(1,101):
         if(number%3==0):
-            print(number)
+            print(number,end=' ')
 
 
 #get multiples of Four
 def multiples_Of_Four():
     for number in range(1,101):
         if(number%4==0):
-            print(number)
+            print(number,end=' ')
 
 
 #get multiples of Nine
 def multiples_Of_Nine():
     for number in range(1,101):
         if(number%9==0):
-            print(number)
+            print(number,end=' ')
--- multiplesTester.py
+++ multiplesTester.py
@@ -1,9 +1,9 @@
 import multiples
 print("Multiples of Two \n")
 multiples.multiples_Of_Two()
-print("\n Multiples of Four\n")
+print("\n Multiples of Four \n")
 multiples.multiples_Of_Four()
-print("\n Multiples of Nine")
+print("\n Multiples of Nine \n")
 multiples.multiples_Of_Nine()
-print("\n Multiples of Three")
+print("\n Multiples of Three \n")
 multiples.multiples_Of_Three()
