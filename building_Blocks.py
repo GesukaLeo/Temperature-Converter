8dec6692009fcd745ec07c3e01665f6f66a95a6c
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-08-06 14:34:56 +0300

    updated the building_blocks.py with format strings

--- building_Blocks.py
+++ building_Blocks.py
@@ -5,4 +5,4 @@ message = "I have a dream"
 school_motto ='Aim Higher'
 
 #print the strings to the screen using .format
-print("My favorite quote is {0} and the message that entrills me is {1}. My school motto was {2} back in the dem day \n".format(Quot,message,school_motto))
+print("My favorite quote is {0} and the message that entrills me is {1}. My school motto was {2} back in the dem day \n".format(Quote,message,school_motto))
