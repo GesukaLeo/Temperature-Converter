d41ee82bf58efc8ea589ba470847b3c1cab398cb
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-08-06 11:51:10 +0300

    added another file called building_blocks.py

--- packages.py
+++ packages.py
@@ -3,10 +3,3 @@
 from math import *
 fact = factorial(5)
 print("The factorial of number 5! is = :",fact)
-
-
-#check if its Not a Number NaN
-test = float('nan')
-x=isnan(test)
-print(test)
-print(x)
--- packagesTester.py
+++ packagesTester.py
@@ -3,5 +3,12 @@ from math import *
 #check if its Not a Number NaN
 test = float('nan')
 x=isnan(test)
-print(test)
-print(x)
+print("The boolean result is :",test)
+print("The ultimate result is now :",x)
+
+
+#check for otherwise
+test2 = float(3456)
+y=isnan(test2)
+print(test2)
+print(y)
