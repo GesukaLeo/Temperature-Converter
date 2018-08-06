39284e93037452778f215bdfd17f1d70f7180af3
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-08-06 11:58:43 +0300

    just printed my strings using .format to the screen

---
+++ building_Blocks.py
@@ -0,0 +1,8 @@
+#codes for showing my experience with strings
+
+Quote = "Never ever give up"
+message = "I have a dream"
+school_motto ='Aim Higher'
+
+#print the strings to the screen using .format
+print("My favorite quote is {0} and the message that entrills me is {1}. My school motto was {2} back in the dem day \n".format(Quot,message,school_motto))
