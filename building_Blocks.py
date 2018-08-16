07aa65dbabc9f84cbd3f5e43aa08021271426a07
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-08-16 08:46:56 +0300

    updated the packages and building blocks

--- building_Blocks.py
+++ building_Blocks.py
@@ -6,3 +6,20 @@ school_motto ='Aim Higher'
 
 #print the strings to the screen using .format
 print("My favorite quote is {0} and the message that entrills me is {1}. My school motto was {2} back in the dem day \n".format(Quote,message,school_motto))
+
+
+x=list(reversed([2,4,6,8,10]))
+print(x)
+
+y=sorted(list({4,2,8,6,10}), reverse=True)
+print(y)
+
+z=list(range(10,0,-2))
+
+print(z)
+
+
+total = 0
+for i in 1, 3, 7:
+   total = total + i
+   print(total)
--- packages.py
+++ packages.py
@@ -3,3 +3,25 @@
 from math import *
 fact = factorial(5)
 print("The factorial of number 5! is = :",fact)
+
+#isogram code
+def is_isogram(string):
+    if type(string) != str:
+        raise TypeError('Argument should be a string')
+    elif string == " ":
+        return (string, False)
+    else:
+        for i in string:
+            if string.count(i) > 1:
+                return (string, False)
+    return (string, True)
+
+
+#call the is_isogram function
+
+x= is_isogram("gesuka")
+y = is_isogram("posses")
+z=is_isogram(12345)
+print(x)
+print(y)
+print(z)
