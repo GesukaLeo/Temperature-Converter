d974c756a7ff4f23af9b157c38ec7986bba01125
GesukaLeo <36921762+GesukaLeo@users.noreply.github.com>
2018-08-16 11:36:29 +0300

    Updated my dictinary with 3 new items

--- packagesTester.py
+++ packagesTester.py
@@ -12,3 +12,35 @@ test2 = float(3456)
 y=isnan(test2)
 print(test2)
 print(y)
+
+#dictionaries methods
+
+shopping_List = {
+    "Eggs":20.00,
+    "Bread":50.00,
+    "Blue Band":250.25,
+    "Juice Mango": 80.40,
+    "Apple":40.24,
+    "tissue":100,
+    "mango":30.50
+}
+
+#print our items in the dictionary
+print(shopping_List)
+
+#values only
+print(shopping_List.values())
+
+#Keys only
+print(shopping_List.keys())
+
+#iterate on the dictionary
+for i in shopping_List:
+    print("%s  %2f" %(i, shopping_List[i]))
+
+#compute the total of values in the dictionary
+Total =0
+for item in shopping_List:
+    Total += shopping_List[item]
+print("Total=",Total)
+
